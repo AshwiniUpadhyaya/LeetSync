7dc08889e7f9b7ef5d2d982f0fa0a625fd18931d
Ashwini <166228916+AshwiniUpadhyaya@users.noreply.github.com>
2026-08-22 15:33:11 +0530

    Time: 468 ms (68.92%) | Memory: 0B (100.00%) - LeetSync

--- 3782-find-valid-emails/find-valid-emails.sql
+++ 3782-find-valid-emails/find-valid-emails.sql
@@ -1,4 +1,2 @@
 # Write your MySQL query statement below
-SELECT user_id, email
-FROM Users
-WHERE email REGEXP '^[a-z0-9_]+@[^@0-9]+\\.com$' order by user_id;
+SELECT user_id, email FROM Users WHERE email REGEXP '^[A-Za-z0-9_]+@[A-Za-z]+\\.com$' ORDER BY user_id;
